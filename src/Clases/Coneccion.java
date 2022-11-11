ccc429eaeb145a3a44ee0f17090dce02a294a9fc
Sebas122005 <sebastianbarca03@gmail.com>
2022-11-10 21:50:36 -0500

    avanze 1

---
+++ src/Clases/Coneccion.java
@@ -0,0 +1,22 @@
+
+package Clases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class Coneccion {
+    
+     public static Connection conectar() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/ec3_modeloycalidad", "root", "12345678");
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error de conecccion : "+e.getMessage());
+        }
+        return con;
+    }
+    
+}
